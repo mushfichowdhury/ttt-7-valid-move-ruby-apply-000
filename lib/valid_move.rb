f421bfe6d7f172735cd48fe42ce1726fc88069b3
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-12 16:58:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,9 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  if position_taken?
+    
+
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
